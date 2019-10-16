400aa56806bba09e9c4788796ecd23b8a77722e5
TERVER AOSU <terveraosu@gmail.com>
2019-10-16 19:00:18 +0100

    calculator components setup

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+
+import Display from "./Display";
+import ButtonPanel from "./ButtonPanel";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is the calculator app</h1>
+      <Display value={'0'} />
+      <ButtonPanel />
+    </div>
+  );
+}
+
+export default App;
